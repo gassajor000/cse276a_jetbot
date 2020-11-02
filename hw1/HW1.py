16943829827a0967c025d5f0348ce6de85e064c7
MARAUDERSMAP\gassa <lgassjsg@gmail.com>
2020-11-01 16:39:03 -0800

    Read waypoints from file.

--- hw1/HW1.py
+++ hw1/HW1.py
@@ -6,20 +6,28 @@ import time
 
 from WallE import WallE
 
+def drive_to_waypoints(walle):
+    # open way points file
+    waypoints = []
+    with open('waypoints.txt') as fl:
+        for line in fl.readline():
+            x, y, theta = line.split(',')
+            waypoints.append((x, y, theta))
+
+    for point in waypoints:
+        x, y, theta = point
+        walle.drive_to(x, y, theta)
+        time.sleep(1)
+
 if __name__ == '__main__':
     W = WallE()
     W.calibrate()
     input('press any key to start sequence')
-    W.drive_to(-1, 0, 0)
-    time.sleep(1)
-    W.drive_to(-1, 1, 1.57)
-    time.sleep(1)
-    W.drive_to(-2, 1, 0)
+    # W.drive_to(-1, 0, 0)
+    # time.sleep(1)
+    # W.drive_to(-1, 1, 1.57)
+    # time.sleep(1)
+    # W.drive_to(-2, 1, 0)
 
-    # # open way points file
-    # waypoints = []
-    #
-    # for point in waypoints:
-    #     x, y, theta = point
-    #     WALLE.drive_to(x, y, theta)
-    #     time.sleep(1)
+    drive_to_waypoints(W)
+
