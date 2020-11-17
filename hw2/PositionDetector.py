5d9ee93be32c0a25f165a2d62a336c709f51cc1f
MARAUDERSMAP\gassa <lgassjsg@gmail.com>
2020-11-16 18:33:13 -0800

    Use fisheye calibration for undistortion.

--- hw2/PositionDetector.py
+++ hw2/PositionDetector.py
@@ -82,52 +82,58 @@ class PositionDetector:
         self.model = ObjectDetector(model_path + 'ssd_mobilenet_v2_coco.engine')
         self.camera = Camera.instance(width=300, height=300)
 
-        self.mtx = [[369.09432263,   0.,         153.06911056],
-                    [  0.,         592.39571459, 153.86106711],
-                    [  0.,           0.,           1.,        ]]
-        self.dist = [[-2.50853423,  6.30981973, -0.03309406,  0.01974386, -8.89580458]]
-
+        # TODO camera calibration
     def calibrate_camera(self, file_path):
         """Run camera calibration on captured images"""
         print('Beginning Camera Calibration')
-        # camera calibration
-        # termination criteria
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+        CHECKERBOARD = (6,9)
+        subpix_criteria = (cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER, 30, 0.1)
+        calibration_flags = cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC+cv2.fisheye.CALIB_CHECK_COND+cv2.fisheye.CALIB_FIX_SKEW
+        objp = numpy.zeros((1, CHECKERBOARD[0]*CHECKERBOARD[1], 3), numpy.float32)
+        objp[0,:,:2] = numpy.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
+        _img_shape = None
+        objpoints = [] # 3d point in real world space
+        imgpoints = [] # 2d points in image plane.
 
-        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-        objp = numpy.zeros((6 * 9, 3), numpy.float32)
-        objp[:, :2] = numpy.mgrid[0:9, 0:6].T.reshape(-1, 2)
-
-        # Arrays to store object points and image points from all the images.
-        objpoints = []  # 3d point in real world space
-        imgpoints = []  # 2d points in image plane.
-
-        images = glob.glob(file_path + '*.jpg')
-        gray = None
+        images = glob.glob('images/*.jpg')
         for fname in images:
-            print('Processing Image ' + fname)
             img = cv2.imread(fname)
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+            if _img_shape == None:
+                _img_shape = img.shape[:2]
+            else:
+                assert _img_shape == img.shape[:2], "All images must share the same size."
+            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
             # Find the chess board corners
-            ret, corners = cv2.findChessboardCorners(gray, (6, 9), None)
-            print('found ret {}'.format(ret, corners))
+            ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
             # If found, add object points, image points (after refining them)
             if ret == True:
                 objpoints.append(objp)
+                cv2.cornerSubPix(gray,corners,(3,3),(-1,-1),subpix_criteria)
+                imgpoints.append(corners)
 
-                corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-                imgpoints.append(corners2)
-
-                # Draw and display the corners
-                # img = cv2.drawChessboardCorners(img, (6, 9), corners2, ret)
-                # cv2.imwrite(fname + '_processed.jpg', img)
-        print("obj size {}, img size {}".format(len(objpoints), len(imgpoints)))
-        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-        self.mtx = mtx
-        self.dist = dist
-        print('Camera Matrix ', mtx)
-        print('Distortion ', dist)
+        N_OK = len(objpoints)
+        K = numpy.zeros((3, 3))
+        D = numpy.zeros((4, 1))
+        rvecs = [numpy.zeros((1, 1, 3), dtype=numpy.float64) for i in range(N_OK)]
+        tvecs = [numpy.zeros((1, 1, 3), dtype=numpy.float64) for i in range(N_OK)]
+        rms, _, _, _, _ = \
+            cv2.fisheye.calibrate(
+                objpoints,
+                imgpoints,
+                gray.shape[::-1],
+                K,
+                D,
+                rvecs,
+                tvecs,
+                calibration_flags,
+                (cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER, 30, 1e-6)
+            )
+        print("Found " + str(N_OK) + " valid images for calibration")
+        print("DIM=" + str(_img_shape[::-1]))
+        print("K=np.array(" + str(K.tolist()) + ")")
+        print("D=np.array(" + str(D.tolist()) + ")")
+        self.K = K
+        self.D = D
 
         self.undistort_image(cv2.imread(images[0]))
 
@@ -149,16 +155,11 @@ class PositionDetector:
 
     def undistort_image(self, image):
         """undistort the image using the calibrated camera matrix"""
-        h, w = image.shape[:2]
-        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(self.mtx, self.dist, (w, h), 1, (w, h))
-        mapx, mapy = cv2.initUndistortRectifyMap(self.mtx, self.dist, None, newcameramtx, (w, h), 5)
-        dst = cv2.remap(image, mapx, mapy, cv2.INTER_LINEAR)
+        map1, map2 = cv2.fisheye.initUndistortRectifyMap(self.K, self.D, numpy.eye(3), self.K, image.shape[:2], cv2.CV_16SC2)
+        undistorted_img = cv2.remap(image, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
 
-        # crop the image
-        x, y, w, h = roi
-        dst = dst[y:y + h, x:x + w]
-        cv2.imwrite('images/calibresult.png', dst)
-        return dst
+        # cv2.imwrite('images/calibresult.png', undistorted_img)
+        return undistorted_img
 
     def get_position(self, distance, rotation):
         """
